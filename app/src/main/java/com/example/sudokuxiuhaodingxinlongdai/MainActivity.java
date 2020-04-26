5838f3fa8aa902304dddd62d0d8417a53f1e77a8
Xiuhao Ding <xiuhaod2@illinois.edu>
2020-04-26 18:37:31 -0500

    4 26 version 2

--- app/src/main/java/com/example/sudokuxiuhaodingxinlongdai/MainActivity.java
+++ app/src/main/java/com/example/sudokuxiuhaodingxinlongdai/MainActivity.java
@@ -4,28 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import static android.R.layout.activity_list_item;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
-    int[][] k = new int[9][9];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        k = (int[][]) getIntent().getExtras().get("array");
+        int[][] k = (int[][]) getIntent().getExtras().get("array");
         GridView gw = findViewById(R.id.map);
         int[] arraOne = oneandtwo.one(k);
         Integer[] arraOneInteger = InttoInteger.cii(arraOne);
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, arraOneInteger);
         gw.setAdapter(adapter);
         Button chk = findViewById(R.id.check);
+        gw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                arraOne[position] += 1;
+                arraOne[position] %= 10;
+                if (arraOne[position] == 0) {
+                    arraOne[position] += 1;
+                }
+                Integer[] arraOneInteger2 = InttoInteger.cii(arraOne);
+                ArrayAdapter<Integer> adapter2 = new ArrayAdapter<Integer>(MainActivity.this, android.R.layout.simple_list_item_1, arraOneInteger2);
+                gw.setAdapter(adapter2);
+            }
+        });
         chk.setOnClickListener(unused -> {
-            int[][] checking = check.change(k);
+            int[][] c = oneandtwo.two(arraOne);
+            int[][] checking = check.change(c);
             if (check.ck(checking) == false) {
                 startActivity(new Intent(this, ending.class));
             }
